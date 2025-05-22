f9066bc049ed28a989c2c0b601f619d6ee637455
Howard Freeman <sgusev6435@gmail.com>
2025-05-22 10:03:19 +0300

    feat: add app header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { BookListWithSearchInput, ReadBookList } from "@/widgets/book-list";
 export default function Home() {
   return (
     <>
-      <h1>Read List</h1>
       <BookListWithSearchInput />
       <ReadBookList />
     </>
